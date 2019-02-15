27c2352ccc31636003a5fd8400db854c0f9d3636
MariaCaicedo <camila19970426@gmail.com>
2019-02-15 15:37:05 -0500

    Update ConjuntoLista.java
    se agregan metodos

--- Conjuntos/src/conjuntos/ConjuntoLista.java
+++ Conjuntos/src/conjuntos/ConjuntoLista.java
@@ -2,8 +2,10 @@ package conjuntos;
 
 import sun.text.resources.ja.CollationData_ja;
 
-public class ConjuntoLista {
-    Nodo cabeza;
+public class
+
+ConjuntoLista {
+    private Nodo cabeza;
 
     public ConjuntoLista() {
         this.cabeza = null;
@@ -37,7 +39,30 @@ public class ConjuntoLista {
 
     public ConjuntoLista union(ConjuntoLista conjuntoB) {
         ConjuntoLista nuevoConjunto = new ConjuntoLista();
+        nuevoConjunto.add(cabeza);
+        nuevoConjunto.add(conjuntoB.getCabeza());
         return nuevoConjunto;
+    }
+
+    private void add(Nodo nodo) {
+        Nodo ultimo = getUltimo();
+        if (ultimo == null) {
+            this.cabeza = nodo;
+        } else {
+            ultimo.setLiga(nodo);
+        }
+    }
+
+    private Nodo getUltimo() {
+        if (this.cabeza == null) {
+            return null;
+        }
+        Nodo recorre = this.cabeza;
+
+        while (recorre.getLiga() != null) {
+            recorre = recorre.getLiga();
+        }
+        return recorre;
     }
 
     public ConjuntoLista interseccion(ConjuntoLista conjuntoB) {
@@ -46,6 +71,32 @@ public class ConjuntoLista {
     }
 
     public boolean igualdad(ConjuntoLista conjuntoB) {
+        if (this.cabeza == null || null == conjuntoB.getCabeza()) {
+            return this.getCabeza() == conjuntoB.getCabeza();
+        }
+        boolean flag = false;
+        Nodo recorre = this.cabeza;
+        while (recorre != null) {
+            Nodo recorreOtro = conjuntoB.getCabeza();
+            boolean encontrado = conjuntoB.buscar(recorre.getDato());
+            if (!encontrado) {
+                return false;
+            }
+            recorre = recorre.getLiga();
+        }
+        return recorre == null;
+    }
+
+    private boolean buscar(char dato) {
+        if (this.cabeza != null) {
+            Nodo recorre = this.cabeza;
+            while (recorre != null) {
+                if (recorre.getDato() == dato) {
+                    return true;
+                }
+                recorre = recorre.getLiga();
+            }
+        }
         return false;
     }
 
@@ -62,6 +113,21 @@ public class ConjuntoLista {
     }
 
     public boolean borrar(char caracter) {
+        if (this.cabeza != null && !buscar(caracter)) {
+            return false;
+        }
+        if (this.cabeza.getDato() == caracter) {
+            this.cabeza = this.cabeza.getLiga();
+        }
+        Nodo recorre = this.cabeza;
+        while (recorre.getLiga() != null) {
+            Nodo anterior = recorre;
+            recorre = recorre.getLiga();
+            if (recorre.getDato() == caracter) {
+                anterior = recorre.getLiga();
+                break;
+            }
+        }
         return false;
     }
 
